3e195655f8c4cb234a34dad3e613a8dec1f804d9
mounishdodda <76152848+mounishdodda@users.noreply.github.com>
2021-05-14 21:48:01 +0530

    Add files via upload

---
+++ reaminder.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+
+if __name__== "__main__":
+    while(True):
+
+
+         notification.notify(
+         title="please rest your eyes",
+         message="Spending too many hours staring at a screen can cause eye strain.  ",
+        ''' timeout=2'''
+        ) 
+        '''time.sleep(60)'''
